e5be2263ea9cf2ef2102c0d99b8b7035d63fe9e6
Alejandro <luigis07>
2020-03-21 09:08:18 -0600

    Implement "Delete User". Close #7

--- Spring-Boot/src/main/java/com/spring/boot/service/UserServiceImpl.java
+++ Spring-Boot/src/main/java/com/spring/boot/service/UserServiceImpl.java
@@ -47,7 +47,7 @@ public class UserServiceImpl implements UserService {
 
 	@Override
 	public User getUserById(Long id) throws Exception {
-		return repository.findById(id).orElseThrow(() -> new Exception("El usuario para editar no existe"));
+		return repository.findById(id).orElseThrow(() -> new Exception("El usuario no existe"));
 	}
 
 	@Override
@@ -68,5 +68,11 @@ public class UserServiceImpl implements UserService {
 		to.setLastName(from.getLastName());
 		to.setEmail(from.getEmail());
 		to.setRoles(from.getRoles());
+	}
+
+	@Override
+	public void deleteUser(Long id) throws Exception {
+		User user = getUserById(id);
+		repository.delete(user);
 	}
 }
